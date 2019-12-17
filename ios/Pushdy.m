8998bfa3c40bc67f79974a17479f584692790b46
luatnd <luatnd@mobiletech.vn>
2019-12-17 14:01:15 +0700

    Ios support sampleMethod with promise, backed by Swift instead of Objective-C

--- ios/Pushdy.m
+++ ios/Pushdy.m
@@ -1,14 +1,32 @@
-#import "Pushdy.h"
+//#import "Pushdy.h"
+
+//@implementation Pushdy
+//
+//RCT_EXPORT_MODULE()
+
+//RCT_EXPORT_METHOD(sampleMethod:(NSString *)stringArgument numberParameter:(nonnull NSNumber *)numberArgument callback:(RCTResponseSenderBlock)callback)
+//{
+//    // TODO: Implement some actually useful functionality
+//    callback(@[[NSString stringWithFormat: @"numberArgument22: %@ stringArgument: %@", numberArgument, stringArgument]]);
+//}
+
+//@end
 
 
-@implementation Pushdy
+/**
+ * Change to use Swift instead of Objective-C
+ * This guide might be useful for you:
+ * https://teabreak.e-spres-oh.com/swift-in-react-native-the-ultimate-guide-part-1-modules-9bb8d054db03#f662
+ */
+#import <React/RCTBridgeModule.h>
 
-RCT_EXPORT_MODULE()
+@interface RCT_EXTERN_MODULE(Pushdy, NSObject)
 
-RCT_EXPORT_METHOD(sampleMethod:(NSString *)stringArgument numberParameter:(nonnull NSNumber *)numberArgument callback:(RCTResponseSenderBlock)callback)
-{
-    // TODO: Implement some actually useful functionality
-    callback(@[[NSString stringWithFormat: @"numberArgument: %@ stringArgument: %@", numberArgument, stringArgument]]);
-}
+RCT_EXTERN_METHOD(
+  sampleMethod: (NSString *)stringArgument
+  numberArgument: (nonnull NSNumber *)numberArgument
+  resolve: (RCTPromiseResolveBlock)resolve
+  rejecter: (RCTPromiseRejectBlock)reject
+)
 
 @end
