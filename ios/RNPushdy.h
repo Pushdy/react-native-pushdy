d174682be3797cd7b33429f737aee3e0bb5e4b19
luatnd <abc.nguyenluat@gmail.com>
2020-10-19 09:27:22 +0700

    Fix duplicate RNPushdy class in new build system

--- ios/RNPushdy.h
+++ ios/RNPushdy.h
@@ -5,7 +5,7 @@
 
 // --- If you use Swift, you don't need header anymore
 // @interface Pushdy : UIResponder <UIApplicationDelegate, RCTBridgeModule>
-@interface Pushdy : RCTEventEmitter <RCTBridgeModule>
+@interface RNPushdyInterface : RCTEventEmitter <RCTBridgeModule>
 +(void)doSthFoo:(NSString*)msg;
 //+(void)initWithContext:(NSString*)clientKey
 //               delegate:(UIApplication *)delegate didFinishLaunchingWithOptions:(NSDictionary *)launchOptions;
